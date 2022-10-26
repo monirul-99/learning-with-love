a78eedd35801dfbc01738d37d7ac00d8d913043f
monaulic <sohelmuhammad164@gmail.com>
2022-10-27 03:35:03 +0600

    facebbok login provider remove

--- src/Components/Pages/SignIn/SignIn.js
+++ src/Components/Pages/SignIn/SignIn.js
@@ -12,12 +12,8 @@ import Swal from "sweetalert2";
 
 const SignIn = () => {
   const [error, setError] = useState(null);
-  const {
-    signIn,
-    handleGoogleProvider,
-    handleGithubProvider,
-    handleFacebookProvider,
-  } = useContext(AuthContext);
+  const { signIn, handleGoogleProvider, handleGithubProvider } =
+    useContext(AuthContext);
 
   // console.log(user.photoURL);
   const navigate = useNavigate();
@@ -25,20 +21,9 @@ const SignIn = () => {
   const from = location.state?.from?.pathname || "/";
 
   const googleMama = new GoogleAuthProvider();
-  const facebookMama = new FacebookAuthProvider();
   const githubMama = new GithubAuthProvider();
   const googleSignIn = () => {
     handleGoogleProvider(googleMama)
-      .then((result) => {
-        const user = result.user;
-        console.log(user);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  };
-  const facebookSignIn = () => {
-    handleFacebookProvider(facebookMama)
       .then((result) => {
         const user = result.user;
         console.log(user);
@@ -149,15 +134,6 @@ const SignIn = () => {
                 >
                   <path d="M16.318 13.714v5.484h9.078c-0.37 2.354-2.745 6.901-9.078 6.901-5.458 0-9.917-4.521-9.917-10.099s4.458-10.099 9.917-10.099c3.109 0 5.193 1.318 6.38 2.464l4.339-4.182c-2.786-2.599-6.396-4.182-10.719-4.182-8.844 0-16 7.151-16 16s7.156 16 16 16c9.234 0 15.365-6.49 15.365-15.635 0-1.052-0.115-1.854-0.255-2.651z"></path>
                 </svg>
-              </button>
-              <button
-                onClick={facebookSignIn}
-                ariaLabel="Log in with facebook"
-                className="p-3 rounded-sm"
-              >
-                <IconContext.Provider value={{ size: 22 }}>
-                  <FaFacebook />
-                </IconContext.Provider>
               </button>
               <button
                 onClick={githubSignIn}
